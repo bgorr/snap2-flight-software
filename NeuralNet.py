ec3b13334c9df524d437c4c6116b64ca65592cd8
Ben Gorr <ben@DESKTOP-N5KVBCE>
2023-05-19 13:33:48 -0500

    Making sim settings through JSON, adding multiple neural nets

--- NeuralNet.py
+++ NeuralNet.py
@@ -15,7 +15,7 @@ def tile_images(large_img):
 
 def untile_mask(small_images):
     full_mask = np.zeros(shape=(512,512))
-    for i in range(0,3):
+    for i in range(0,4):
         for j in range(0,3):
             full_mask[j*161:(j+1)*161,i*105:(i+1)*105] = small_images[3*i+j]
         full_mask[483:512,i*105:(i+1)*105] = small_images[3*i+3][132:161,0:105]
@@ -35,7 +35,7 @@ def get_centroid(image):
                 x_sum = x_sum + i
                 y_sum = y_sum + j
                 count = count + 1
-    #print(count)
+    print("Number of plume pixels: "+count)
     if count > 0 and count < 1000:
         print("Plume detected!")
         x_loc = x_sum/count
@@ -50,26 +50,94 @@ def preprocess(bands):
     return bands
 
 class NeuralNet():
-    def __init__(self):
-        self.nn = torch.jit.load("./trainednet.pt")
-    
-    def perform_inference(self,input):
-        output = self.nn.forward(input)
+    def __init__(self,nn_setting,num_bands):
+        if num_bands == 3:
+            if nn_setting == "skeptical":
+                self.nn = torch.jit.load("./trainednet_3s.pt")
+            else:
+                self.nn = torch.jit.load("./trainednet_3o.pt")
+        elif num_bands == 4:
+            if nn_setting == "skeptical":
+                self.nn = torch.jit.load("./trainednet_4s.pt")
+            else:
+                self.nn = torch.jit.load("./trainednet_4o.pt")
+        else:
+            if nn_setting == "skeptical":
+                self.nn = torch.jit.load("./trainednet_5s.pt")
+            else:
+                self.nn = torch.jit.load("./trainednet_5o.pt")
+        self.nn_3s = torch.jit.load("./trainednet_3s.pt")
+        self.nn_4s = torch.jit.load("./trainednet_4s.pt")
+        self.nn_5s = torch.jit.load("./trainednet_5s.pt")
+        self.nn_3o = torch.jit.load("./trainednet_3o.pt")
+        self.nn_4o = torch.jit.load("./trainednet_4o.pt")
+        self.nn_5o = torch.jit.load("./trainednet_5o.pt")
+    def perform_inference_3bands_skeptical(self,input):
+        output = self.nn_3s.forward(input)
         outputSigmoid = torch.sigmoid(output)
-        outputThreshold = torch.gt(outputSigmoid,0.66)
+        outputThreshold = torch.gt(outputSigmoid,0.4)
+        return outputThreshold[0][0] 
+    def perform_inference_3bands_optimistic(self,input):
+        output = self.nn_3o.forward(input)
+        outputSigmoid = torch.sigmoid(output)
+        outputThreshold = torch.gt(outputSigmoid,0.4)
+        return outputThreshold[0][0] 
+    def perform_inference_4bands_optimistic(self,input):
+        output = self.nn_4o.forward(input)
+        outputSigmoid = torch.sigmoid(output)
+        outputThreshold = torch.gt(outputSigmoid,0.4)
+        return outputThreshold[0][0]
+    def perform_inference_4bands_skeptical(self,input):
+        output = self.nn_4s.forward(input)
+        outputSigmoid = torch.sigmoid(output)
+        outputThreshold = torch.gt(outputSigmoid,0.4)
+        return outputThreshold[0][0]
+    def perform_inference_5bands_skeptical(self,input):
+        output = self.nn_5s.forward(input)
+        outputSigmoid = torch.sigmoid(output)
+        outputThreshold = torch.gt(outputSigmoid,0.4)
+        return outputThreshold[0][0]
+    def perform_inference_5bands_optimistic(self,input):
+        output = self.nn_5o.forward(input)
+        outputSigmoid = torch.sigmoid(output)
+        outputThreshold = torch.gt(outputSigmoid,0.4)
         return outputThreshold[0][0]
     
-    def run_nn(self,image):
-        processed_bands = preprocess(image)
+    def run_nn(self,vnir_image,swir_image):
+        processed_bands = preprocess(vnir_image)
+        swir_processed = preprocess(swir_image)
         red_tiles = tile_images(processed_bands[0])
         green_tiles = tile_images(processed_bands[1])
         blue_tiles = tile_images(processed_bands[2])
         nir_tiles = tile_images(processed_bands[3])
-        mask_tiles = []
+        swir_tiles = tile_images(swir_processed)
+        mask_tiles_3s = []
+        mask_tiles_3o = []
+        mask_tiles_4s = []
+        mask_tiles_4o = []
+        mask_tiles_5s = []
+        mask_tiles_5o = []
         for i in range(20):
-            stacked_tensor = torch.stack((torch.from_numpy(red_tiles[i]).float(),torch.from_numpy(green_tiles[i]).float(),torch.from_numpy(blue_tiles[i]).float(),torch.from_numpy(nir_tiles[i]).float())) # np has float64, need float
+            stacked_tensor_3bands = torch.stack((torch.from_numpy(red_tiles[i]).float(),torch.from_numpy(green_tiles[i]).float(),torch.from_numpy(blue_tiles[i]).float())) # np has float64, need float
+            stacked_tensor_4bands = torch.stack((torch.from_numpy(red_tiles[i]).float(),torch.from_numpy(green_tiles[i]).float(),torch.from_numpy(blue_tiles[i]).float(),torch.from_numpy(nir_tiles[i]).float())) # np has float64, need float
+            stacked_tensor_5bands = torch.stack((torch.from_numpy(red_tiles[i]).float(),torch.from_numpy(green_tiles[i]).float(),torch.from_numpy(blue_tiles[i]).float(),torch.from_numpy(nir_tiles[i]).float(),torch.from_numpy(swir_tiles[i]).float())) # np has float64, need float
             #final_tensor = torch.stack((stacked_tensor))
-            mask_tiles.append(self.perform_inference(torch.unsqueeze(stacked_tensor,0)))
-        full_mask = untile_mask(mask_tiles)
-        (x,y) = get_centroid(full_mask)
-        return full_mask, x, y
+            mask_tiles_3o.append(self.perform_inference_5bands_optimistic(torch.unsqueeze(stacked_tensor_3bands,0)))
+            mask_tiles_3s.append(self.perform_inference_5bands_skeptical(torch.unsqueeze(stacked_tensor_3bands,0)))
+            mask_tiles_4o.append(self.perform_inference_5bands_optimistic(torch.unsqueeze(stacked_tensor_4bands,0)))
+            mask_tiles_4s.append(self.perform_inference_5bands_skeptical(torch.unsqueeze(stacked_tensor_4bands,0)))
+            mask_tiles_5o.append(self.perform_inference_5bands_optimistic(torch.unsqueeze(stacked_tensor_5bands,0)))
+            mask_tiles_5s.append(self.perform_inference_5bands_skeptical(torch.unsqueeze(stacked_tensor_5bands,0)))
+        full_mask_3s = untile_mask(mask_tiles_3s)
+        (x_3s,y_3s) = get_centroid(full_mask_3s)
+        full_mask_3o = untile_mask(mask_tiles_3o)
+        (x_3o,y_3o) = get_centroid(full_mask_3o)
+        full_mask_4s = untile_mask(mask_tiles_4s)
+        (x_4s,y_4s) = get_centroid(full_mask_4s)
+        full_mask_4o = untile_mask(mask_tiles_4o)
+        (x_4o,y_4o) = get_centroid(full_mask_4o)
+        full_mask_5s = untile_mask(mask_tiles_5s)
+        (x_5s,y_5s) = get_centroid(full_mask_5s)
+        full_mask_5o = untile_mask(mask_tiles_5o)
+        (x_5o,y_5o) = get_centroid(full_mask_5o)
+        return full_mask_5s, x_5s, y_5s
